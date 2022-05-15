262f4a3abcc0359d61bbcb4d1d4e6d8c54edc102
Evgeny Dolganev <dolganev007@yandex.ru>
2022-05-15 20:31:37 +0700

    sp 15 5

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,12 +1,20 @@
-const allowedCors = 'http://mesto.dolganev.nomoredomains.xyz';
-
 const cors = (req, res, next) => {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
   }
 
-  next();
+  return next();
 };
 
 module.exports = cors;
