0bdb58c383123ffced7197b7c49591b790beaf77
Evgeny Dolganev <dolganev007@yandex.ru>
2022-05-15 20:26:06 +0700

    sp 15 4

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = 'http://mesto.dolganev.nomoredomains.xyz/';
+const allowedCors = 'http://mesto.dolganev.nomoredomains.xyz';
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
